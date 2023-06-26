fde42b0f0fcd3928633cbed6e31250fdf6576b71
Usman Liaqat <137144786+OrbixtarTechnologies@users.noreply.github.com>
2023-06-26 08:47:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
-const app = express()
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
     res.send('App is live now')
 })
 app.listen(process.env.PORT || 3000)
